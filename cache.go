0cf423172c6b843e5cfe3347cb9de9434bbfba45
ixuzl <863582605@qq.com>
2019-03-26 16:58:53 +0800

    更新

--- cache.go
+++ cache.go
@@ -1,7 +1,15 @@
-package go_cache
+package cache
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+)
+
+var Path string ="runtime/cache/"
 
 type CachenIterface interface {
-	Set(string,interface{},int64)bool
+	Set()bool
 	Get() string
 	//IsExists()
 }
@@ -19,8 +27,22 @@ func (f File)Get(name string) string {
 }
 
 func (f File) Set(key string ,value interface{},time int64) bool{
+	path:=genPath(key)
+	fmt.Println(path)
 	return true
 }
+func genFileName(name string) string {
+	hash:=md5.New()
+	hash.Write([]byte(name))
+	resu:=hash.Sum(nil)
+	return hex.EncodeToString(resu)
+}
+func genPath(name string)string  {
+	str:=genFileName(name)
+	path:=Path+str[:2]+"/"+str[2:]
+	fmt.Println(path)
+}
+
 
 
 
@@ -32,10 +54,10 @@ func (f File) Set(key string ,value interface{},time int64) bool{
 //	//return
 //}
 
-func (c *File)Set(key string, value string,time int64)  {
-	
-}
-
-func (c *File )IsExists() bool {
-
-}
+//func (c *File)Set(key string, value string,time int64)  {
+//
+//}
+//
+//func (c *File )IsExists() bool {
+//
+//}
--- main.go
+++ main.go
@@ -1,35 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"net"
+	"os"
+)
 
-type Animal interface {
-	Speak() string
-}
-
-type Cat struct{}
-func (c Cat) Speak() string {
-	return "cat"
-}
-
-type Dog struct{}
-func (d Dog) Speak() string {
-	return "dog"
-}
-func (d Dog) Set() string {
-	return "set"
-}
-
-func Test(params interface{}) {
-	fmt.Println(params)
-}
+var Path string = "runtime/cache/"
 
 func main() {
-	animals := []Animal{Cat{}, Dog{}}
-	for _, animal := range animals {
-		fmt.Println(animal.Speak())
+	fmt.Println(getMacAddrs())
+	//Set("1234567", 123213, 3600)
+}
+
+func Set(key string, value interface{}, time int64) bool {
+	path := genPath(key)
+	fmt.Println(path)
+	return true
+}
+func genFileName(name string) string {
+	hash := md5.New()
+	hash.Write([]byte(name))
+	resu := hash.Sum(nil)
+	return hex.EncodeToString(resu)
+}
+func genPath(name string) string {
+	str := genFileName(name)
+	path := Path + str[:2]+"/"
+	os.MkdirAll(path,os.ModePerm)
+	fmt.Println(path)
+	return ""
+}
+
+func getMacAddrs() (macAddrs []string) {
+	netInterfaces, err := net.Interfaces()
+	fmt.Println(netInterfaces)
+	if err != nil {
+		fmt.Printf("fail to get net interfaces: %v", err)
+		return macAddrs
 	}
 
-	Test("string")
-	Test(123)
-	Test(true)
+	for _, netInterface := range netInterfaces {
+		macAddr := netInterface.HardwareAddr.String()
+		if len(macAddr) == 0 {
+			continue
+		}
+
+		macAddrs = append(macAddrs, macAddr)
+	}
+	return macAddrs
 }
